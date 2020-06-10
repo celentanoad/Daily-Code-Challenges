4e986cce60c0284ae2eca7f15bb5e2c151c7d5f1
Alanna Celentano <celentanoad91@gmail.com>
2020-06-10 11:19:26 -0500

    solve steps problem

---
+++ steps/steps.js
@@ -0,0 +1,35 @@
+// --- Directions
+// Write a function that accepts a positive number N.
+// The function should console log a step shape
+// with N levels using the # character.  Make sure the
+// step has spaces on the right hand side!
+// --- Examples
+//   steps(2)
+//       '# '
+//       '##'
+//   steps(3)
+//       '#  '
+//       '## '
+//       '###'
+//   steps(4)
+//       '#   '
+//       '##  '
+//       '### '
+//       '####'
+
+//Recursive Solution
+function steps(n, row = 0, stair = '') {
+    if (n === row) return;
+    if (stair.length === n) {
+        console.log(stair);
+        return steps(n, row + 1);
+    }
+    if (stair.length <= row) {
+        stair += '#';
+    } else {
+        stair += ' ';
+    }
+    steps(n, row, stair);
+}
+
+console.log(steps(10))
